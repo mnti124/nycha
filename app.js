9015d005113d881a33ca13c3d12b131c27f81573
Michael Nti <michaelnti@Michaels-MBP-2.home>
2020-05-14 21:47:49 -0400

    Uploading all files & Folder

---
+++ app.js
@@ -0,0 +1,7 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  particlesJS.load('particles2-js', 'particles2.json', function() {
+    console.log('callback - particles2.js config loaded');
+  });
